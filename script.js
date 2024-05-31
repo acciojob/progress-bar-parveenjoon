2d9c8e3924e6e132151c5478a0ccf59da764134c
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 09:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentCircleIndex = 0;
+
+// Initially only the first circle should have the active class
+circles[currentCircleIndex].classList.add('active');
+
+// Add click event listener to next button
+nextButton.addEventListener('click', () => {
+    if (currentCircleIndex < circles.length - 1) {
+        currentCircleIndex++;
+        circles[currentCircleIndex].classList.add('active');
+        circles[currentCircleIndex - 1].classList.remove('active');
+        prevButton.disabled = false;
+        if (currentCircleIndex === circles.length - 1) {
+            nextButton.disabled = true;
+        }
+    }
+});
+
+// Add click event listener to previous button
+prevButton.addEventListener('click', () => {
+    if (currentCircleIndex >= 1) {
+        currentCircleIndex--;
+        circles[currentCircleIndex].classList.add('active');
+        circles[currentCircleIndex + 1].classList.remove('active');
+        nextButton.disabled = false;
+        if (currentCircleIndex === 0) {
+            prevButton.disabled = true;
+        }
+    }
+});
